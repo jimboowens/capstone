6fc4af1d5700bc4ec4431b6d293c619476af1094
Jeremy Owens <jeremyowens@Jeremys-MacBook-Pro-2.local>
2019-03-13 19:26:28 -0400

    trying this

---
+++ back-end/public/precache-manifest.46fa5c67b862caacc21fa62f04bed317.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c96867f432f635aeb019",
+    "url": "/static/css/main.e33496ed.chunk.css"
+  },
+  {
+    "revision": "c96867f432f635aeb019",
+    "url": "/static/js/main.c96867f4.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "938d4426689107647736",
+    "url": "/static/css/2.7d6ae2c9.chunk.css"
+  },
+  {
+    "revision": "938d4426689107647736",
+    "url": "/static/js/2.938d4426.chunk.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "22dd36f616c2ece8a4b1cc2217095003",
+    "url": "/static/media/Simplifica.22dd36f6.ttf"
+  },
+  {
+    "revision": "a43662fea0a023a0fe55301a3a5a6315",
+    "url": "/index.html"
+  }
+];
