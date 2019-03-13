b3e526e9be63783763833f0fd09357254625ef8d
“jimboowens” <jimcowens@gmail.com>
2019-03-13 13:38:46 -0400

    working through all issues making site as bug-free as possible

--- front-end/src/components/pages/Contact.js
+++ front-end/src/components/pages/Contact.js
@@ -30,11 +30,11 @@ class Contact extends Component{
                     <p>
                         Your satisfaction is as important to us as it is to you. We are your first line of defense against a less than exceptional experience. 
                     </p>
-                    <span id="email"><a to="mailto:support-patron@ebuy.com">EMAIL US</a></span>
+                    <span id="email"><Link to="mailto:support-patron@ebuy.com">EMAIL US</Link></span>
                     <img src="/images/jim.png" alt="" />
                     <h1>Jim Owens</h1>
                     <Link className="col s3" to="https://github.com/jimboowens"><img src="/images/github.png" alt=""/></Link>
-                    <Link className="col s3" to="https://www.linkedin.com/in/jimcowens/" alt=""><img src="https://static.licdn.com/sc/h/al2o9zrvru7aqj8e1x2rzsrca" /></Link>
+                    <Link className="col s3" to="https://www.linkedin.com/in/jimcowens/" alt=""><img src="https://static.licdn.com/sc/h/al2o9zrvru7aqj8e1x2rzsrca" alt=""/></Link>
                     <p>
                         I am a web developer, recently moved over from management at Panera. I am passionate about solving problems in creative and efficient ways, while making ergonomic and enjoyable websites. Code can be 
                         complex, and solving coding issues aren't the only problem. Making code that others can readily read and understand is critical to contributing to a part of any team. I care about my friends and family, 
--- front-end/src/reducers/searchReducer.js
+++ front-end/src/reducers/searchReducer.js
@@ -5,8 +5,8 @@
 // if you want to change me, let me know by an action.type
 
 export default (state=[],action)=>{
-    console.log(action.type)
-    console.log(action.payload||"no data")
+    // console.log(action.type)
+    // console.log(action.payload||"no data")
     // signature takes state and action (signature is the stuff passed into a function set as a parameter)
     switch (action.type) {
         case "SEARCH_ACTION":
